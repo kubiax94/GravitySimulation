04f937b0180d8d8224e0e325ab8f87a7e4df078f
kubiax94 <kubiax12@gmail.com>
2025-04-28 19:04:16 +0200

    add g scale

--- GravitySimulation/unit_system.cpp
+++ GravitySimulation/unit_system.cpp
@@ -1,5 +1,11 @@
 #include "unit_system.h"
 
+const float unit_system::const_G = 6.67430e-11f;
+
+const float unit_system::scaled_G() const {
+	return const_G * (mass_scale / (distance_scale * distance_scale));
+}
+
 float unit_system::mass(float kg) {
 	return kg / mass_scale;
 }
--- GravitySimulation/unit_system.h
+++ GravitySimulation/unit_system.h
@@ -1,6 +1,8 @@
 #pragma once
 struct unit_system
 {
+	static const float const_G;
+	const float scaled_G() const;
 	float mass_scale;
 	float distance_scale;
 	float scale_time;
@@ -8,6 +10,10 @@ struct unit_system
 	float mass(float kg);
 	float distance(float km);
 	float time(float seconds);
+
+	float to_renderer_scale(float realm_km) {
+		return realm_km / 12742.0f;
+	}
 
 };
 
