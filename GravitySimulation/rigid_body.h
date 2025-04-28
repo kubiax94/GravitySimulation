9d14daba139d9f637b9ae8103a940e078f982b8c
kubiax94 <kubiax12@gmail.com>
2025-04-28 19:01:03 +0200

    added data

--- GravitySimulation/rigid_body.h
+++ GravitySimulation/rigid_body.h
@@ -5,8 +5,9 @@
 struct physics_data
 {
 	float mass = 1.f;
-	glm::vec3 velocity = glm::vec3(0.f, 1.f, 0);
-	glm::vec3 accumulated_force = glm::vec3(1.f);
+	glm::vec3 position = glm::vec3(0.f);
+	glm::vec3 velocity = glm::vec3(0.f, 0.f, 0);
+	glm::vec3 accumulated_force = glm::vec3(0.f);
 };
 
 class rigid_body : public component, public physics_data
