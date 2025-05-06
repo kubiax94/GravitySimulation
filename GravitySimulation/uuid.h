aeaaa3065d41f5d4b723857dc5b364db36b3b7bf
kubiax94 <kubiax12@gmail.com>
2025-05-06 21:09:33 +0200

    add hash metod

--- GravitySimulation/uuid.h
+++ GravitySimulation/uuid.h
@@ -14,7 +14,19 @@ public:
 
 	bool operator==(const uuid& other) const;
 	bool operator!=(const uuid& other) const;
-	operator uint64_t() const;
+	explicit operator uint64_t() const;
 
 	std::string to_string() const;
 };
+
+namespace std
+{
+	template<>
+	struct hash<uuid>
+	{
+		std::size_t operator()(const uuid& u) const noexcept {
+			return std::hash<uint64_t>()(static_cast<uint64_t>(u));
+		}
+	};
+
+}
