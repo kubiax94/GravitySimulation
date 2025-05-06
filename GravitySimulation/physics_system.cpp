159b9c9d37d005da3eb01643a75797c3e39f1324
kubiax94 <kubiax12@gmail.com>
2025-05-06 21:13:23 +0200

    compute shader integration

--- GravitySimulation/physics_system.cpp
+++ GravitySimulation/physics_system.cpp
@@ -1,21 +1,38 @@
 ﻿#include "physics_system.h"
 
+#include <glm/gtx/string_cast.hpp>
+
 //float scale_mass = 1e24f; // masa Ziemi
 //float scale_distance = 1e6f; // 1mln km
 //float scale_time = 3.872e6f / 3600.f; // 1h
 
-physics_system::physics_system(unit_system* u_sys): u_sys(u_sys) {
+std::vector<physics_data> physics_system::get_physics_data() const {
+
+	std::vector<physics_data> data(order_id_.size());
+
+	for (auto i = 0; i < order_id_.size(); i++)
+		data[i] = static_cast<physics_data>(*entities_.at(order_id_[i]));
+
+	return  data;
+}
+
+physics_system::physics_system(unit_system* u_sys): u_sys_(u_sys) {
+	gravity_simulation_comp_ = new compute_shader("gravity_simulation.glsl");
 }
 
 bool physics_system::add(rigid_body* r_body) {
-	auto* test = r_body->get_node();
-	entities_[test] = r_body;
+	
+	auto* node = r_body->get_node();
+
+	if (entities_.contains(node->get_id())) return false;
+	entities_[node->get_id()] = r_body;
+	order_id_.push_back(node->get_id());
 
 	return true;
 }
 
-void physics_system::gravity_simulation() {
-	std::vector<std::pair<scene_node*, rigid_body*>> all(entities_.begin(), entities_.end());
+void physics_system::gravity_simulation_cpu() {
+	std::vector<std::pair<uuid, rigid_body*>> all(entities_.begin(), entities_.end());
 
 	for (size_t i = 0; i < all.size(); ++i)
 	{
@@ -28,9 +45,9 @@ void physics_system::gravity_simulation() {
 			float dist = glm::length(dir);
 
 			if (dist < 1e-3f) continue;
-
+			
 			glm::vec3 force_dir = glm::normalize(dir);
-			float force_mag = u_sys->scaled_G() * a_body->mass * b_body->mass / (dist * dist);
+			float force_mag = u_sys_->scaled_G() * a_body->get_mass() * b_body->get_mass() / (dist * dist);
 			glm::vec3 force = force_dir * force_mag;
 
 			a_body->apply_force(force);
@@ -39,9 +56,29 @@ void physics_system::gravity_simulation() {
 	}
 }
 
+void physics_system::gravity_simulation_gpu() {
+
+	auto gpu_data = get_physics_data();
+
+	gravity_simulation_comp_->update_ssbo(gpu_data);
+	auto gpu_result = gravity_simulation_comp_->compute(entities_.size());
+
+	for (auto i = 0; i < order_id_.size(); i++) {
+		entities_[order_id_[i]]->position = gpu_result[i].position;
+		entities_[order_id_[i]]->velocity = gpu_result[i].velocity;
+		entities_[order_id_[i]]->accumulated_force = gpu_result[i].accumulated_force;
+
+	}
+}
+
+
 bool physics_system::remove(rigid_body* rigid_body) {
-	if (entities_.contains(rigid_body->get_node())) {
-		entities_.erase(rigid_body->get_node());
+
+	uuid node_id = rigid_body->get_node()->get_id();
+
+	if (entities_.contains(node_id)) {
+		entities_.erase(node_id);
+		
 
 		return true;
 	}
@@ -51,9 +88,15 @@ bool physics_system::remove(rigid_body* rigid_body) {
 
 void physics_system::update(const float& dt) {
 
-	gravity_simulation();
+	//gravity_simulation_cpu();
+	gravity_simulation_comp_->set_uni_float("G", u_sys_->scaled_G());
+	gravity_simulation_gpu();
 
-	for (auto& entity : entities_ | std::views::values)
+	glm::vec3 total_momentum = glm::vec3(0);
+	for (auto& entity : entities_ | std::views::values) {
 		entity->integrate(dt);
+		total_momentum += entity->get_mass() * entity->velocity;
+	}
 
+	std::cout << glm::to_string(total_momentum) << std::endl;
 }
--- GravitySimulation/physics_system.h
+++ GravitySimulation/physics_system.h
@@ -1,18 +1,27 @@
 ﻿#pragma once
+#include "compute_shader.h"
 #include "rigid_body.h"
 #include "unit_system.h"
+#include "uuid.h"
 
 class physics_system
 {
-	std::unordered_map<scene_node*, rigid_body*> entities_;
-	unit_system* u_sys;
+	std::unordered_map<uuid, rigid_body*> entities_;
+	std::vector<uuid> order_id_;
+
+	unit_system* u_sys_;
+
+	compute_shader* gravity_simulation_comp_;
+
+	std::vector<physics_data> get_physics_data() const;
 
 public:
 	physics_system(unit_system* u_sys);
 
 	bool add(rigid_body* r_body);
-	void gravity_simulation();
-	;
+	void gravity_simulation_cpu();
+	void gravity_simulation_gpu();
+	
 	bool remove(rigid_body* rigid_body);
 
 	void update(const float& dt);
