bc8192be4bfdaf0eda160b5b9be4130ff4fe862e
kubiax94 <kubiax12@gmail.com>
2025-05-06 21:12:54 +0200

    compute shader opt

--- GravitySimulation/rigid_body.cpp
+++ GravitySimulation/rigid_body.cpp
@@ -8,6 +8,23 @@ type_id_t rigid_body::type_id() {
 
 rigid_body::rigid_body(scene_node* owner, const physics_data& p_data) : component(owner), physics_data(p_data) {}
 
+rigid_body::rigid_body(scene_node* owner, const physics_data* p_data) : component(owner), physics_data(*p_data) {
+}
+
+void rigid_body::set_mass(const float& n_mass) {
+	position.w = n_mass;
+	velocity.w = n_mass;
+	accumulated_force.w = n_mass;
+}
+
+glm::vec3 rigid_body::get_position() const {
+	return position;
+}
+
+float rigid_body::get_mass() const {
+	return position.w;
+}
+
 type_id_t rigid_body::get_type_id() const {
 	return type_id();
 }
@@ -21,11 +38,21 @@ void rigid_body::attach_to(scene_node* n_node) {
 }
 
 void rigid_body::apply_force(const glm::vec3& force) {
-	accumulated_force += force;
+	accumulated_force += glm::vec4(force, 0.f);
 }
 
 void rigid_body::clear_force() {
-	accumulated_force = glm::vec3(0.f);
+	accumulated_force = glm::vec4(glm::vec3(0.f), get_mass());
+}
+
+void rigid_body::apply_to_scene() const {
+	if (get_node())
+		get_node()->set_global_position(this->position);
+}
+
+void rigid_body::set_position(const glm::vec3& n_pos) {
+	float mass = get_mass();
+	position = glm::vec4(n_pos, mass);
 }
 
 bool rigid_body::detach() {
@@ -37,12 +64,12 @@ bool rigid_body::detach() {
 }
 
 void rigid_body::integrate(const float& dt) {
-	if (mass <= 0.f) return;
+	if (get_mass() <= 0.f) return;
 
-	glm::vec3 acceleration = accumulated_force / mass;
-	velocity += acceleration * dt;
+	glm::vec3 acceleration = accumulated_force / get_mass();
+	velocity += glm::vec4(acceleration * dt, 0.f);
 
-	position += velocity * dt;
+	position += glm::vec4(glm::vec3(velocity * dt), 0.f);
 
 	if (get_node())
 		get_node()->set_global_position(position);
--- GravitySimulation/rigid_body.h
+++ GravitySimulation/rigid_body.h
@@ -1,14 +1,8 @@
 ﻿#pragma once
 
 #include "Component.h"
+#include "physics_data.h"
 
-struct physics_data
-{
-	float mass = 1.f;
-	glm::vec3 position = glm::vec3(0.f);
-	glm::vec3 velocity = glm::vec3(0.f, 0.f, 0);
-	glm::vec3 accumulated_force = glm::vec3(0.f);
-};
 
 class rigid_body : public component, public physics_data
 {
@@ -18,10 +12,19 @@ private:
 public:
 	static type_id_t type_id();
 	rigid_body(scene_node* owner, const physics_data& p_data);
+	rigid_body(scene_node* owner, const physics_data* p_data);
 
 	void attach_to(scene_node* n_node) override;
 	void apply_force(const glm::vec3& force);
 	void clear_force();
+
+	void apply_to_scene() const;
+
+	void set_position(const glm::vec3& n_pos);
+	void set_mass(const float& n_mass);
+
+	glm::vec3 get_position() const;
+	float get_mass() const;
 
 	type_id_t get_type_id() const override;
 
